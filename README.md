10762fefcba8537c72de2056416b8abe60c3504a
Michael Hahn <skipiano@gmail.com>
2022-09-25 19:09:29 -0400

    update program and readme

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # sheet-copier
 Copy two google sheets from each other
+
+## Instructions
+
+*Note: currently only supported on MacOS*
+
+1. [Install python](https://www.python.org/ftp/python/3.10.7/python-3.10.7-macos11.pkg)
+2. Open the terminal window by searching the spotlight.
+
+![Terminal](https://i.imgur.com/MLhmGxe.png)
+
+3. Install the required dependency by running the following command on terminal:
+```bash
+$ pip install pynput
+```
+4. Open the source airtable and the destination google sheet on tab 1 and 2 respectively. Make sure both of the cells are at the top left corner where the copying should start.
+
+![Tabs](https://i.imgur.com/z3tJtM7.png)
+
+5. Download the zip file of this repository: `Code > Download ZIP`
+![Download](https://i.imgur.com/7UzQbiU.png)
+
+6. Go to `Settings > Security & Privacy & Accessibility` and grant permission to `Python`.
+![Settings](https://i.imgur.com/iq2e3Nf.png)
+
+7. Open the file `main.py` with the IDLE application.
+![IDLE](https://i.imgur.com/zUKCOoz.png)
+
+8. **Make sure you are on the source airtable before starting the program!**
+![Airtable](https://i.imgur.com/ZjJ9xk5.png)
+
+9. `Run > Run Module` to run the program. You will be prompted to enter the amount of rows you want to copy.
+![Rows](https://i.imgur.com/jBssJj9.png)
+
+11. You will have 5 seconds to click back onto your browser so that the program starts copying properly.
+![Running](https://i.imgur.com/9sqoeqY.png)
+
+12. Force quit IDLE if you want to stop the copying operation midway. (`Cmd + opt + esc`)
+![Force Quit](https://i.imgur.com/6sTDpnz.png)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import time
-from pynput.keyboard import Key, Controller
+import sys
+from pynput.keyboard import Key, Controller, Listener
 
 delay = 0.25
-rows = 454
+rows = 0
 cols = 13
 control = Controller()
 
@@ -10,7 +11,7 @@ control = Controller()
 def dove():
     control.press(Key.cmd)
     time.sleep(delay)
-    control.tap('5')
+    control.tap('1')
     time.sleep(delay)
     control.release(Key.cmd)
 
@@ -18,7 +19,7 @@ def dove():
 def sheet():
     control.press(Key.cmd)
     time.sleep(delay)
-    control.tap('4')
+    control.tap('2')
     time.sleep(delay)
     control.release(Key.cmd)
 
@@ -46,8 +47,33 @@ def move_down():
         control.press(Key.left)
 
 
+def on_press(key):
+    if key == 'q':
+        print("Terminating program")
+        sys.exit()
+
+
 if __name__ == "__main__":
-    time.sleep(2)
+    while(True):
+        try:
+            rows = int(input("Enter the number of rows you want to copy: "))
+        except ValueError:
+            print("Please enter a valid integer.")
+            continue
+        else:
+            print("Input received.")
+            result_time = 35*rows
+            h = result_time//3600
+            m = (result_time % 3600)//60
+            s = result_time % 60
+            print(
+                "Approximately {} hours, {} minutes, {} seconds to finish execution".format(h, m, s))
+            print(
+                "Please click back to your browser onto your source sheet to allow the program to start copying")
+            break
+
+    time.sleep(5)
+    print("Starting")
     for i in range(rows):
         for j in range(cols-1):
             time.sleep(delay)
